7eb634999028da0b0171f5ac02a750522e22c5b3
Prinku2810 <102411464+Prinku2810@users.noreply.github.com>
2022-05-05 15:27:27 +0530

    Create Register.py

---
+++ Register.py
@@ -0,0 +1,24 @@
+import re
+import pickle
+import Data
+#Register
+def Register():
+      username=input("Enter Email id  :")
+      password=input("Enter Password  :")
+
+      username_check=r"([^0-9_.+-][][a-z0-9A-Z._+-a]+@[a-zA-Z0-9.-]+\.[a-z])"
+      match_username=re.match(username_check,username)
+
+      if match_username==None:
+        print("Entered username is not valid")
+
+      password_check="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*_+-])[a-zA-Z\d!@#$%^&*_+-]{5,16}$"
+      match_password=re.match(password_check,password)
+
+      if match_password==None:
+        print("Password entered is not valid")
+     
+      user_pswd.update({username:password})
+     
+      with open("Register.txt",'wb') as regis:
+        pickle.dump(user_pswd,regis)
