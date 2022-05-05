464261e65efd793b97c2253460f32779dff7a30e
Prinku2810 <102411464+Prinku2810@users.noreply.github.com>
2022-05-05 16:21:18 +0530

    Update Forgotpswd.py

--- Forgotpswd.py
+++ Forgotpswd.py
@@ -1,5 +1,6 @@
 import re
 import pickle
+import Loginsys
 
 def forgot_password():
   user=input("Enter Username   :")
@@ -9,5 +10,6 @@ def forgot_password():
     d=pickle.loads(data)
   if (d.get(user)==None):
     print("Username doesnot exists, kindly register again")
+    Loginsys.login_sys()
   else:  
     print("your password is  :",d.get(user))
